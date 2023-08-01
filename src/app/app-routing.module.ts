32f785c28a4b589d979b97ee9256b27cf99fd22d
Gustrevasx <gustavobreno44@gmail.com>
2023-08-01 15:11:35 -0400

    1 version complet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MenuComponent } from './pages/menu/menu.component';
+
 import { HomeComponent } from './pages/home/home.component';
-import { ContactComponent } from './pages/contact/contact.component';
+
 import { AboutComponent } from './pages/about/about.component';
 
 //Cria as rotas das paginas os caminhos
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'menu', component: MenuComponent},
-  { path: 'contact', component: ContactComponent },
+
   { path: 'about', component: AboutComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavibarComponent } from './sharedpage/navibar/navibar.component';
 import { FooterComponent } from './sharedpage/navibar/footer/footer.component';
-import { MenuComponent } from './pages/menu/menu.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
     NavibarComponent,
     FooterComponent,
-    MenuComponent,
+
     HomeComponent,
     AboutComponent,
-    ContactComponent
+
+
   ],
   imports: [
     BrowserModule,
